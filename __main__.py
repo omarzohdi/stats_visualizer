3bc3fc13d6f51a127ac5fcc2a2d4808caf8a518c
Omar Zohdi <omarzohdi@gmail.com>
2023-02-21 21:17:49 +0000

    Sets up script for github actions

--- __main__.py
+++ __main__.py
@@ -1,23 +1,21 @@
 import argparse
+import os
 from statsvisualizer import StatsVisualizer as statVis
 
-def cli():
-    # This is the result of a -h command
-    parser = argparse.ArgumentParser(description="Welcome to the Stats Collector! - "
-                                                 "Here you'll be able to collect stats for Github and Google analytics")
-    parser.add_argument("--repo", help="URL of The Github repository to collect data from", default="A")
-    args = parser.parse_args()
 
 
 if __name__ == "__main__":
-    #cli()  # todo add cli integration here.
+    try:
+        GITHUB_TOKEN = os.environ["GITHUB_TOKEN"]
+    except KeyError:
+        print("No GITHUB_TOKEN environment variable found")
+        #GITHUB_TOKEN = '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'
 
-    repo_url_web = "omarzohdi/omarzohdi.github.io"
-    repo_url_sot = "omarzohdi/Soteria"
-    token = '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'
+    repo_url_web = "powervr-graphics/Native_SDK"
+    repo_url_sot = "powervr-graphics/PowerVR-Series1"
 
     sv = statVis()
-    sv.init_github_user_info(token)
+    sv.init_github_user_info(GITHUB_TOKEN)
     sv.add_github_repo_info(repo_url_web, output_dir='stats')
     sv.add_github_repo_info(repo_url_sot, output_dir='stats')
     sv.collect_all_repos_stats()
